50b201bd777b04c805dcc3d0a7c2a493642f9776
thegoodjakeward <75824649+thegoodjakeward@users.noreply.github.com>
2022-10-07 03:29:44 -0400

    Add files via upload

--- 3d/script.lua
+++ 3d/script.lua
@@ -18,39 +18,69 @@ Ball = model3d.load(files.cdir().."/3d/data/ball.obj")
 Water = model3d.load(files.cdir().."/3d/data/plane.obj")
 Ramp = model3d.load(files.cdir().."/3d/data/ramp.obj")
 
-model3d.setphysics(Ball,1,{0,0,0},{0,0,0},2,__CONVEX)
-model3d.setphysics(Water,1,{0,0,0},{0,0,0},0,__CONVEX)
+model3d.setphysics(Ball,1,{0,0,0},{0,0,0},2,__SPHERE)
+model3d.setphysics(Water,1,{0,0,0},{0,0,0},0,__BOX)
 model3d.setphysics(Ramp,1,{0,0,0},{0,0,0},0,__CONVEX)
 
 model3d.physics(Ball)
 model3d.physics(Water)
 model3d.physics(Ramp)
 
-model3d.setdamping(Ball,1,0.1,0.1)
+model3d.setdamping(Ball,1,0.4,1000000)
 model3d.position(Ball,1,{-2,6,0})
 
 Rotation = 0;
+RotationForCalc = 0;
+SinSign = 0;
+CosSign = 0;
+tempVz = 0;
+tempVx = 0;
 
 while true do
 	amg.begin()
-	amg.gravity(0,-9.8,0) --Gravedad
-	
-	if Rotation > 2*math.pi then Rotation -= 2*math.pi
-	elseif Rotation < -2*math.pi then Rotation += 2*math.pi end
-	model3d.rotation(Ball,1,{0,Rotation,0})
+	amg.gravity(0,-9.8,0)
 	
 	BallP = model3d.getposition(Ball,1)
 	BallV = model3d.getvelocity(Ball,1)
-	BallR = model3d.getrotation(Ball,1)
-	if BallV.x > 10  then BallV.x = 10  end
-	if BallV.x < -10 then BallV.x = -10 end
-	if BallV.z > 10  then BallV.z = 10  end
-	if BallV.z < -10 then BallV.z = -10 end
-
-	cam3d.position(camera1,{BallP.x,BallP.y+3,BallP.z})
-	cam3d.eye(camera1,{100*math.sin(Rotation)+BallP.x,BallP.y,100*math.cos(Rotation)+BallP.z})
-	cam3d.rotation(camera1,{0,-Rotation,0})
 	
+	if Rotation > 2*math.pi then Rotation=0
+	elseif Rotation < 0 then Rotation = 2*math.pi end
+	
+	cam3d.position(camera1,{BallP.x,BallP.y+3,BallP.z})
+	cam3d.eye(camera1,{10*math.sin(Rotation)+BallP.x,BallP.y+3,10*math.cos(Rotation)+BallP.z})
+	
+	if Rotation < math.pi/2 then 
+		RotationForCalc = Rotation
+		SinSign = 1
+		CosSign = 1
+	elseif Rotation < math.pi then 
+		RotationForCalc = math.pi - Rotation
+		SinSign = 1
+		CosSign = -1
+	elseif Rotation < 3*math.pi/2 then 
+		RotationForCalc = Rotation-math.pi
+		SinSign = -1
+		CosSign = -1		
+	else 
+		RotationForCalc = 2*math.pi - Rotation 
+		SinSign = -1
+		CosSign = 1
+	end
+	
+	if (BallV.x*BallV.x + BallV.z*BallV.z) > 400  then 
+		BallV.z = BallV.z*0.97
+		BallV.x = BallV.x*0.97
+	end
+
+	buttons.read()
+	if buttons.held.up then model3d.setvelocity(Ball,1,{BallV.x+0.5*SinSign*math.sin(RotationForCalc),BallV.y,BallV.z+0.5*CosSign*math.cos(RotationForCalc)}) end
+	if buttons.held.down then model3d.setvelocity(Ball,1,{BallV.x-0.5*SinSign*math.sin(RotationForCalc),BallV.y,BallV.z-0.5*CosSign*math.cos(RotationForCalc)}) end
+	if buttons.held.left then model3d.setvelocity(Ball,1,{BallV.x+0.5*CosSign*math.cos(RotationForCalc),BallV.y,BallV.z-0.5*SinSign*math.sin(RotationForCalc)}) end
+	if buttons.held.right then model3d.setvelocity(Ball,1,{BallV.x-0.5*CosSign*math.cos(RotationForCalc),BallV.y,BallV.z+0.5*SinSign*math.sin(RotationForCalc)}) end
+	if buttons.held.cross and math.abs(BallV.y) < 0.05 then model3d.setvelocity(Ball,1,{BallV.x,15,BallV.z}) end
+	if buttons.held.l then Rotation += 0.0300630876 end
+	if buttons.held.r then Rotation -= 0.0300630876 end
+
 	--Set camara
 	cam3d.set(camera1)
 
@@ -60,33 +90,21 @@ while true do
 	model3d.render(Water)
 
 	model3d.blitshadow(Ball,1,1,100,1)
-	model3d.render(Ball)
+	--model3d.render(Ball)
 
 	amg.light(1,0);--desactivar luz 1
 
 	amg.mode2d(1)
-	screen.print(15,168,"Rotation"..Rotation)
+	screen.print(15,168,"Rotation"..RotationForCalc)
 	screen.print(15,180,"X-Position"..BallP.x)
 	screen.print(15,192,"Y-Position"..BallP.y)
 	screen.print(15,204,"Z-Position"..BallP.z)
-	screen.print(15,216,"X-Velocity"..BallV.x)
-	screen.print(15,228,"Y-Velocity"..BallV.y)
-	screen.print(15,240,"Z-Velocity"..BallV.z)
+	screen.print(15,216,"Speed"..math.sqrt(BallV.x*BallV.x + BallV.z*BallV.z))
+	screen.print(15,228,"TempVz"..tempVz)
+	screen.print(15,240,"TempVx"..tempVx)
 	if buttons.start then dofile("script.lua") end
 	amg.mode2d(0)
 	screen.flip()
-
-	buttons.read()
-	if buttons.held.up then model3d.setvelocity(Ball,1,{BallV.x+math.sin(Rotation),BallV.y,BallV.z+math.cos(Rotation)}) end
-	if buttons.held.down then model3d.setvelocity(Ball,1,{BallV.x-math.sin(Rotation),BallV.y,BallV.z-math.cos(Rotation)}) end
-	if buttons.held.left then model3d.setvelocity(Ball,1,{BallV.x+math.cos(Rotation),BallV.y,BallV.z-math.sin(Rotation)}) end
-	if buttons.held.right then model3d.setvelocity(Ball,1,{BallV.x-math.cos(Rotation),BallV.y,BallV.z+math.sin(Rotation)}) end
-	if buttons.held.cross and BallP.y < 1.2 then 
-		model3d.setvelocity(Ball,1,{BallV.x,15,BallV.z}) 
-	end
-	if buttons.held.l then Rotation += 0.03 end
-	if buttons.held.r then Rotation -= 0.03 end
-	model3d.rotation(Ball,1,{0,Rotation,0})
 	
 	amg.update()
 	model3d.updatephysics()
